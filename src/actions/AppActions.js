c1e6c4cb6cbbc70381743e41ec18fb0d644d1fe5
DaryaTarasowa <darya.tarasowa@gmail.com>
2015-03-08 23:37:30 +0100

    bug fixed

--- src/actions/AppActions.js
+++ src/actions/AppActions.js
@@ -1,20 +1,15 @@
 module.exports = {
-     loadLanguages: function(context){         
+            
      loadLanguages: function(context){         
         context.service.read('deck.google_languages', {}, {}, function(err, res) {
           if (err) {
             context.dispatch('GOOGLE_LANGUAGES_FAILURE', err);
-            
-        
             return;
           }
           context.dispatch('GOOGLE_LANGUAGES_SUCCESS', {
             languages: res.languages
           });
      
-          
-         
-       
         });       
     }
 }
